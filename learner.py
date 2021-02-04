86d4ccf86c774c91c7f444651d8db3d7bf140814
jorchamo <jorge.chamorro@t-systems.com>
2021-02-04 17:05:44 +0100

    Add learner and data preparer

--- learner.py
+++ learner.py
@@ -1,5 +1,7 @@
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
 
 def category(centrality: float, density: float) -> str:
     if(centrality < 0):
@@ -15,4 +17,16 @@ def category(centrality: float, density: float) -> str:
 
 dataframe = pd.read_pickle('output/prepared_dataframe.pkl')
 dataframe['category'] = [category(centrality, density) for centrality, density in zip(dataframe['centrality'], dataframe['density'] )]
-print(dataframe['category'].value_counts())
+print(dataframe.head())
+print(dataframe.shape)
+
+
+X = dataframe.iloc[:, :-1].values
+y = dataframe.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(y)
